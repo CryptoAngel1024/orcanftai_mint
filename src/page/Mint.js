8c0c9c7c300f2a65af511d176aea3d50bff4034b
CryptoAngel1024 <cryptoangel1024@gmail.com>
2022-08-09 03:07:51 +0800

    amount_fix

--- src/page/Mint.js
+++ src/page/Mint.js
@@ -10,7 +10,9 @@ const Mint = () => {
   const [totalSupply, setTotalSupply] = useState(0);
   const [preSalePrice, setPreSalePrice] = useState(0);
   const [publicSalePrice, setPublicSalePrice] = useState(0);
-  const [amount, setAmount] = useState(1)
+  const [amount1, setAmount1] = useState(1)
+  const [amount2, setAmount2] = useState(1)
+  const [amount3, setAmount3] = useState(1)
   const web3 = new Web3("https://mainnet.infura.io/v3/202fe8f1f38d4c468742ce31f9ecbd7f")
   const mintContract = new web3.eth.Contract(ABI_MINT, `${process.env.REACT_APP_NFT}`);
   useEffect(() => {
@@ -40,24 +42,24 @@ const Mint = () => {
         <div className="text-2xl text-center font-bold">Minted 0/{totalSupply} Supply</div>
         <div className="text-xl text-center">Mint 1 Pad Club NTF for {preSalePrice} ETH</div>
         <div className="text-xl text-center">Maximum allowed mints per Wallet: 10 NFT</div>
-        <input type="text" className="bg-blue-300 py-2 px-4" value={amount} onChange={(e)=>setAmount(e.target.value)}/>
-        <div className="w-56 mx-auto"><WalletButton price={publicSalePrice} mintType={1} amount={Number(amount)}/> </div>
+        <input type="text" className="bg-blue-300 py-2 px-4" value={amount1} onChange={(e)=>setAmount1(e.target.value)}/>
+        <div className="w-56 mx-auto"><WalletButton price={publicSalePrice} mintType={1} amount={Number(amount1)}/> </div>
       </div>
 
       <div className="text-white border rounded-xl w-1/2 mx-auto flex flex-col p-4 space-y-3 border-blue-460 bg-blue-860">
         <div className="text-2xl text-center font-bold">Minted 0/{totalSupply} Supply</div>
         <div className="text-xl text-center">Mint 1 Pad Club NTF for {publicSalePrice} ETH</div>
         <div className="text-xl text-center">Maximum allowed mints per Wallet: 10 NFT</div>
-        <input type="text" className="bg-blue-300 py-2 px-4" value={amount} onChange={(e)=>setAmount(e.target.value)}/>
-        <div className="w-56 mx-auto"><WalletButton price={preSalePrice} mintType={2} amount={Number(amount)}/> </div>
+        <input type="text" className="bg-blue-300 py-2 px-4" value={amount2} onChange={(e)=>setAmount2(e.target.value)}/>
+        <div className="w-56 mx-auto"><WalletButton price={preSalePrice} mintType={2} amount={Number(amount2)}/> </div>
       </div>
 
       <div className="text-white border rounded-xl w-1/2 mx-auto flex flex-col p-4 space-y-3 border-blue-460 bg-blue-860">
         <div className="text-2xl text-center font-bold">Minted 0/{totalSupply} Supply</div>
         <div className="text-xl text-center">Mint 1 Pad Club NTF for 0 ETH</div>
         <div className="text-xl text-center">Maximum allowed mints per Wallet: 10 NFT</div>
-        <input type="text" className="bg-blue-300 py-2 px-4" value={amount} onChange={(e)=>setAmount(e.target.value)}/>
-        <div className="w-56 mx-auto"><WalletButton price={preSalePrice} mintType={3} amount={Number(amount)}/> </div>
+        <input type="text" className="bg-blue-300 py-2 px-4" value={amount3} onChange={(e)=>setAmount3(e.target.value)}/>
+        <div className="w-56 mx-auto"><WalletButton price={preSalePrice} mintType={3} amount={Number(amount3)}/> </div>
       </div>
     </div>
   );
